2a59a3b908e4dc678092bf6310b863ca6dc7a04e
Mikko Kuitunen <mikko.kuitunen@tekla.com>
2014-10-20 14:29:47 +0300

    Add project 2

---
+++ fs2.glsl
@@ -0,0 +1,22 @@
+#version 330
+smooth in vec4 theColor;
+smooth in vec4 thePosition;
+out vec4 outputColor;
+void main()
+{
+    
+    vec4 origin = vec4(0,0,0,1.0);
+    vec4 diff = thePosition - origin;
+
+    if(length(diff) < 0.1)
+    {
+        outputColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    else
+    {
+        outputColor = theColor;
+    }
+
+    
+
+}
